51f11f45d19050b4b79e1e1d0704899758e02069
Quinn Peterson <qap2001@gmail.com>
2023-05-03 14:59:31 -0700

    fix up database connection

--- src/database.py
+++ src/database.py
@@ -5,12 +5,8 @@ import sqlalchemy
 
 def database_connection_url():
     dotenv.load_dotenv()
-    DB_USER: str = os.environ.get("POSTGRES_USER")
     DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
-    DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
-    DB_PORT: str = os.environ.get("POSTGRES_PORT")
-    DB_NAME: str = os.environ.get("POSTGRES_DB")
-    return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
+    return f"postgresql://postgres:{DB_PASSWD}@db.cilwbqbgvckpilzqjarl.supabase.co:6543/postgres"
 
 # Create a new DB engine based on our connection string
 engine = create_engine(database_connection_url())
