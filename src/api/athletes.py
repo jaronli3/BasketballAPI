ceff5731ead0f66af15ae27ba5acc6adb3aa822b
Quinn Peterson <qap2001@gmail.com>
2023-05-04 13:41:47 -0700

    athletes: get_athlete

--- src/api/athletes.py
+++ src/api/athletes.py
@@ -7,36 +7,42 @@ from src import database as db
 
 router = APIRouter()
 
-
 @router.get("/athletes/{id}", tags=["athletes"])
 def get_athlete(id: int):
     """ 
     This endpoint returns a single athlete by its identifier. For each athlete it returns:
     * `athlete_id`: The internal id of the athlete.
     * `name`: The name of the athlete
-    * `sport`: The sport the athlete plays
-    * `team`: The team the athlete plays for
-    * `gender`: The gender of the athlete
+    * `team_id`: The team id the athlete plays for
     * `age`: The age of the athlete
-    * `years`: the years they played
     * `stats`: a json returning some of the stats of the athlete
-    * `stats` is represented by a dictionary in which the keys are dependent on the sport 
-    the athlete plays. For example, if the athlete plays baseball, stats will include batting average.
+    * games_played, minutes_played, field_goal_percentage, three_point_percentage, free_throw_percentage, total_rebounds, assist, steals, blocks, points 
     """
 
-    return {"gg": 1}
+    stmt = (sqlalchemy.select(db.athletes).where(db.athletes.c.athlete_id == id))
 
-    # with db.engine.connect() as conn:
-    #     result_a = conn.execute(
-    #         sqlalchemy.text("""
-    #         SELECT
-    #             characters.character_id,
-    #             characters.name AS character,
-    #             movies.title AS movie,
-    #             characters.gender
-    #         FROM characters
-    #         JOIN movies ON characters.movie_id = movies.movie_id
-    #         WHERE characters.character_id = :x
-    #         """), [{"x": id}]
-    #     ).fetchone()
+    with db.engine.connect() as conn:
+        res = conn.execute(stmt).fetchone()
+        if res is None:
+            raise HTTPException(status_code=404, detail="athlete not found.")
 
+        stats = {
+            "games_played": res.games_played,
+            "minutes_played": res.minutes_played,
+            "field_goal_percentage": res.field_goal_percentage,
+            "three_points_percentage": res.three_points_percentage,
+            "free_throw_percentage": res.free_throw_percentage,
+            "total_rebounds": res.total_rebounds,
+            "assists": res.assists,
+            "steals": res.steals,
+            "blocks": res.blocks,
+            "points": res.points
+        }
+
+        return {
+            "athlete_id": res.athlete_id,
+            "name": res.name,
+            "team_id": res.team_id,
+            "age": res.age,
+            "stats": stats
+        }
