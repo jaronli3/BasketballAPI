0c7196480ef98b9b222838422b5cf3444f3e7e39
zach <zweinfel@calpoly.edu>
2023-05-05 22:27:10 -0700

    "updated sort order for get_game()"

--- src/api/games.py
+++ src/api/games.py
@@ -41,20 +41,22 @@ class team_options(str, Enum):
     brooklyn_nets = "Brooklyn Nets"
 
 
-@router.get("/games/{id}", tags=["games"])
+@router.get("/games/", tags=["games"])
 def get_game(
         home_team: team_options = team_options.toronto_raptors,
         away_team: team_options = team_options.milwaukee_bucks
 ):
     """
-    This endpoint returns a single game by its identifier
-        *game_id: internal id of game
-        *home_team: name of home team
-        *away_team: name of away team
-        *winner_team: name of winner team
-        *home_team_score: score of home team
-        *away_team_score: score of away team
-        *date: the date the game was held
+    This endpoint returns a list of games by the teams provided ordered by date
+
+    For each game it returns:
+        *`game_id`: internal id of game
+        *`home_team`: name of home team
+        *`away_team`: name of away team
+        *`winner_team`: name of winner team
+        *`home_team_score`: score of home team
+        *`away_team_score`: score of away team
+        *`date`: the date the game was held
     """
     if home_team == away_team:
         raise HTTPException(status_code=400, detail="Teams are the same")
@@ -81,7 +83,11 @@ def get_game(
                 db.games.c.pts_away,
                 db.games.c.date
             ).where((db.games.c.home == home_team_id) & (db.games.c.away == away_team_id))
+                .order_by(db.games.c.date)
         ).fetchall()
+
+        if len(result) == 0:
+            raise HTTPException(status_code=404, detail="No games found")
 
         json = [
             {"game_id": game.game_id,
@@ -94,3 +100,68 @@ def get_game(
             for game in result
         ]
         return json
+
+
+def add_game(
+        home_team: team_options,
+        away_team: team_options,
+        winner: team_options,
+        date: str = None,
+        points_home: int = None,
+        points_away: int = None,
+        three_pointer_percent_home: float = None,
+        three_pointer_percent_away: float = None,
+        rebounds_home: int = None,
+        rebounds_away: int = None,
+        assists_home: int = None,
+        assists_away: int = None,
+        steals_home: int = None,
+        steals_away: int = None,
+        blocks_home: int = None,
+        blocks_away: int = None
+):
+    """
+    This endpoint adds a game to the database. The game is represented by:
+        *`home_team_id`: the id of the home team
+        *`away_team_id`: the id of the away team
+        *`winner_id`: the id of the winner’s team
+        *Additional statistics about the game
+
+    The endpoint returns the id of the game created
+    """
+    if home_team == away_team:
+        raise HTTPException(status_code=400, detail="Teams are the same")
+    if winner != home_team and winner != away_team:
+        raise HTTPException(status_code=400, detail="Select a valid winner")
+
+    with db.engine.connect() as conn:
+        game_id = conn.execute(
+            sqlalchemy.select(
+                db.games.c.game_id
+            )
+            .order_by(sqlalchemy.desc(db.games.c.game_id))
+            .limit(1)
+        ).fetchone().game_id + 1
+
+        home_team_id = conn.execute(
+            sqlalchemy.select(
+                db.teams.c.team_id
+            ).where(db.teams.c.team_name == home_team)
+        ).fetchone().team_id
+
+        away_team_id = conn.execute(
+            sqlalchemy.select(
+                db.teams.c.team_id
+            ).where(db.teams.c.team_name == away_team)
+        ).fetchone().team_id
+
+        game = {
+            "game_id": game_id,
+            "home": home_team_id,
+            "away": away_team_id,
+            "winner": home_team_id if home_team == winner else away_team_id,
+            "date": date,
+
+        }
+
+add_game(team_options.milwaukee_bucks, team_options.toronto_raptors, team_options.milwaukee_bucks)
