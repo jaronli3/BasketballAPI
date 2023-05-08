6b1b935d8da7b9ec982ef80778c756dd5086a7ab
Jaron Li <2002jaron@gmail.com>
2023-05-07 23:25:25 -0700

    done with compare_team

--- src/api/teams.py
+++ src/api/teams.py
@@ -4,6 +4,7 @@ from collections import Counter
 import sqlalchemy
 from fastapi.params import Query
 from src import database as db
+from typing import List, Dict
 
 router = APIRouter()
 
@@ -36,15 +37,10 @@ def get_team(team_id: int):
 
             games_table = conn.execute(games).fetchall()
 
-            # games_won = sqlalchemy.select(db.games.c.winner).where(db.games.c.winner == team_id)
-            # games_won1 = conn.execute(games_won).fetchall()
-
             wins = 0
             points_for = 0
             points_allowed = 0
 
-            # for row in games_won1:
-            #     wins += 1
             for row in games_table:
                 if row.winner == team_id:
                     wins += 1
@@ -73,12 +69,12 @@ class stat_options(str, Enum):
     assists = "assists"
     steals = "steals"
     blocks = "blocks"
-    avg_fg_pct = "average field goal percentage"
+    # avg_fg_pct = "average field goal percentage"
     avg_3p_pct = "average three point percentage"
-    avg_ft_pct = "average free throw percentage"
+    # avg_ft_pct = "average free throw percentage"
 
 @router.get("/teams/", tags=["teams"])
-def compare_team(teams: list, compare_by: stat_options = stat_options.wins):
+def compare_team(teams: List[str] = Query(None), compare_by: stat_options = stat_options.wins):
 
     ''' 
     This endpoint compares any number of teams (> 1) by a single metric 
@@ -90,11 +86,84 @@ def compare_team(teams: list, compare_by: stat_options = stat_options.wins):
             * "assists"
             * "steals"
             * "blocks"
-            * "average field goal percentage"
             * "average three point percentage"
-            * "average free throw percentage"
     '''
 
-    ...
 
+    if len(teams) < 2:
+        raise HTTPException(status_code=400, detail="not enough teams given")
     
+    stmt = (
+        sqlalchemy.select(db.teams.c.team_id, db.teams.c.team_name)
+        .where(sqlalchemy.column('team_name').in_(teams))
+    )
+    
+    with db.engine.connect() as conn:
+        result = conn.execute(stmt).fetchall()
+        json = []
+        for row in result:
+            team_dict = {"team id": row.team_id, "team name": row.team_name}
+            games = sqlalchemy.select(db.games.c.home, 
+                                    db.games.c.pts_home, 
+                                    db.games.c.winner,
+                                    db.games.c.reb_home,
+                                    db.games.c.ast_home,
+                                    db.games.c.stl_home,
+                                    db.games.c.blk_home,
+                                    db.games.c.three_p_percent_home,
+                                    db.games.c.away, 
+                                    db.games.c.pts_away, 
+                                    db.games.c.winner,
+                                    db.games.c.reb_away,
+                                    db.games.c.ast_away,
+                                    db.games.c.stl_away,
+                                    db.games.c.blk_away,
+                                    db.games.c.three_p_percent_away).where((row.team_id == db.games.c.home) | (row.team_id == db.games.c.away))
+
+            games1 = conn.execute(games).fetchall()
+            wins = 0
+            points = 0
+            rebounds = 0 
+            assists = 0
+            steals = 0
+            blocks = 0
+            average_three = 0
+
+            for game in games1:
+                if game.winner == row.team_id:
+                    wins += 1
+                if game.home == row.team_id:
+                    points += game.pts_home
+                    rebounds += game.reb_home
+                    assists += game.ast_home
+                    steals += game.stl_home
+                    blocks += game.blk_home
+                    average_three += game.three_p_percent_home
+                elif game.away == row.team_id:
+                    points += game.pts_away
+                    rebounds += game.reb_away
+                    assists += game.ast_away
+                    steals += game.stl_away
+                    blocks += game.blk_away
+                    average_three += game.three_p_percent_away
+
+            if compare_by == "wins":
+                stats_dict = {"Wins": wins}
+            elif compare_by == "points":
+                stats_dict = {"Average points per game": round((points/82), 2)}
+            elif compare_by == "rebounds":
+                stats_dict = {"Average rebounds per game": round((rebounds/82), 2)}
+            elif compare_by == "assists":
+                stats_dict = { "Average assists per game": round((assists/82),2)}
+            elif compare_by == "steals":
+                stats_dict = { "Average steals per game": round((steals/82), 2)}
+            elif compare_by == "blocks":
+                stats_dict = {  "Average blocks per game": round((blocks/82), 2)}
+            elif compare_by == "average three point percentage":
+                stats_dict = {"Average three point percentage per game": round((average_three/82), 2)}
+            
+            team_dict[f"Compare by {compare_by}"] = stats_dict
+
+            json.append(team_dict)
+        
+        return json
