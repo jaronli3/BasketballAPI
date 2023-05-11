b0f528e05ea6c62a3ea93e398d4dee55a0b87103
Zweinfeld001 <zweinfel@calpoly.edu>
2023-05-11 13:31:51 -0700

    "Removed hardcoding"

--- src/database.py
+++ src/database.py
@@ -12,7 +12,6 @@ def database_connection_url():
     DB_PORT: str = os.environ.get("POSTGRES_PORT")
     DB_NAME: str = os.environ.get("POSTGRES_DB")
     return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
-    # return f"postgresql://postgres:{DB_PASSWD}@db.cilwbqbgvckpilzqjarl.supabase.co:6543/postgres"
 
 # Create a new DB engine based on our connection string
 engine = create_engine(database_connection_url())
