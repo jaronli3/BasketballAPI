a60558b078555a208dc6339bfe679fe683e35b0c
Zweinfeld001 <zweinfel@calpoly.edu>
2023-05-18 13:51:30 -0700

    "Started predictions"

--- src/api/predictions.py
+++ src/api/predictions.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter
 import sqlalchemy
 from src import database as db
 from src.api.teams import team_options
-
+# from fastapi.testclient import TestClient
+# from src.api.server import app
+#
+#
+# client = TestClient(app)
 
 router = APIRouter()
 
@@ -13,6 +17,8 @@ def get_team_market_price(team: team_options):
     This endpoint returns the current market price of the specified team
     """
 
+
+print(get_team_market_price(team_options.toronto_raptors))
 
 @router.get("/predictions/athlete", tags=["predictions"])
 def get_athlete_market_price(athlete: str):
