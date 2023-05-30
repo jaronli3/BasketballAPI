a30f6ebd5b80d03c3bdabe76431237f7a6d971e5
Quinn Peterson <qap2001@gmail.com>
2023-05-30 15:17:02 -0700

    fix error in prev commit

--- src/api/teams.py
+++ src/api/teams.py
@@ -104,6 +104,8 @@ def get_team(team_id: int,
         if result is None:
             raise HTTPException(status_code=404, detail="team not found")
 
+        team_name = result.team_name
+
         if year:
             stats = [get_team_helper(conn, team_id, year)]
         else:
