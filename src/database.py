5e2b2069d5fbecfa3c55dbe3cbbf706ce766bb9c
Jaron Li <2002jaron@gmail.com>
2023-05-31 14:11:55 -0700

    work

--- src/database.py
+++ src/database.py
@@ -11,6 +11,8 @@ def database_connection_url():
     DB_PORT: str = os.environ.get("POSTGRES_PORT")
     DB_NAME: str = os.environ.get("POSTGRES_DB")
     return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
+    # return "postgresql://postgres:postgres@localhost:54322/postgres"
+
 
 
 # Create a new DB engine based on our connection string
